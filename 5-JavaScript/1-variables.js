63678be1b8db4a5186b708829d50ef62a6dfff42
Ajingi1 <ahmadamirued12@gmail.com>
2023-12-05 09:06:12 +0300

    Added JavaSricpt files

---
+++ 5-JavaScript/1-variables.js
@@ -0,0 +1,37 @@
+console.log("Hello World");
+//This is single line comment
+/*This is mulple line comment
+
+*/
+// Statements: JavaScript code is composed of statements that perform specific actions. Each statement typically ends with a semicolon (;), although it is not always required.
+// Comments: You can add comments in JavaScript to provide explanations or make notes within the code. There are two types of comments: single-line comments, starting with //, and multi-line comments, enclosed between /* and */.
+// Case sensitivity: JavaScript is case-sensitive, meaning that uppercase and lowercase letters are treated differently. For example, "variable" and "Variable" would be considered as two separate variables.
+// Whitespace: JavaScript ignores spaces, tabs, and line breaks (collectively called whitespace) that are not necessary for code execution. However, whitespace can enhance code readability.
+// Blocks: Code blocks in JavaScript are enclosed in curly braces ({}) and are used to group statements together. Blocks are often used in control structures like loops and conditional statements.
+// let every time var when necessary
+
+
+// variables
+let name = "John"
+console.log("This is the name of the user", name);
+
+// firstName
+// lastName
+let firstName = "Ahmad";
+let lastName = "Amiru";
+console.log(firstName, " ", lastName);
+
+var myFavoriteGame = "Animal crossing";
+console.log("This is my favorite game", myFavoriteGame);
+
+if (true) {
+    let myFavoriteDrink = "cafe latte";
+    var mySecondFavoriteDrink = "Tea";
+    console.log(myFavoriteDrink);
+}
+// console.log(myFavoriteDrink);
+console.log(mySecondFavoriteDrink);
+
+// non changable variable const
+const gravity = "9.81";
+const birthday = "June 23";
