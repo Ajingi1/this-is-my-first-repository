1cca911bd163628d7a26367f84e80fea69a600da
Ajingi1 <ahmadamirued12@gmail.com>
2023-12-07 09:39:44 +0300

    updated

--- 5-JavaScript/JavaScript-Activity/app.js
+++ 5-JavaScript/JavaScript-Activity/app.js
@@ -2,11 +2,11 @@ let score  = parseInt(prompt("Enter the score"));
 
 if (score >= 90) {
     console.log("The Grade for Score" , score ,"is: 'A'" );
-} else if(score > 80 && score <= 89){
+} else if(score >= 80 && score <= 89){
     console.log("The Grade for Score" , score ,"is: 'B'" );
-}else if(score > 70 && score <= 79){
+}else if(score >= 70 && score <= 79){
     console.log("The Grade for Score" , score ,"is: 'C'" );
-}else if(score > 60 && score <= 69){
+}else if(score >= 60 && score <= 69){
     console.log("The Grade for Score" , score ,"is: 'D'" );
 }else{
     console.log("The Grade for Score" , score ,"is: 'F'" );
